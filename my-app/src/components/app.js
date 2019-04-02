035969ca58934f60d8c204dfb77c032b7a3286e1
Vento <ilya.popov.sbmt@gmail.com>
2019-04-02 15:44:46 +0300

    Client: react app added

---
+++ my-app/src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Messenger from './messenger';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app=wrapper">
+        Hello, World!
+        <Messenger />
+      </div>
+    );
+  }
+}
---
+++ my-app/src/components/messenger.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Messenger extends Component {
+  render() {
+    return <div className="">Hello, from Messenger!</div>;
+  }
+}
